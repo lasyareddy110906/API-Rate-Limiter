be49514d0b9534ac87b45cd62464c0d4a1ae8843
Lasya Reddy <kandadilasya110906@gmail.com>
2026-05-18 15:49:44 +0530

    Fix deployment

--- server.js
+++ server.js
@@ -1,45 +1,19 @@
 import express from "express";
-import { fixedWindowLimiter } from "./middleware/fixedWindowLimiter.js";
-import { slidingWindowLimiter } from "./middleware/slidingWindowLimiter.js";
-import { tokenBucketLimiter } from "./middleware/tokenBucketLimiter.js";
-import { leakyBucketLimiter } from "./middleware/leakyBucketLimiter.js";
-import client from "./config/redis.js";
-import { redisSlidingWindowLimiter } from "./middleware/redisSlidingWindowLimiter.js";
-
 
 const app = express();
 
 app.use(express.json());
 
-// Home Route
 app.get("/", (req, res) => {
-  res.send("API Rate Limiter Running 🚀");
+  res.send("API Rate Limiter Running ");
 });
 
-// Protected Route
-app.get(
-  "/api",
-  redisSlidingWindowLimiter(5, 60000),
-  (req, res) => {
-    res.send(
-      "Redis Sliding Window Request Successful"
-    );
-  }
-);
-const PORT = 9000;
+app.get("/api", (req, res) => {
+  res.send("API Route Working");
+});
 
-const startServer = async () => {
-  try {
-    await client.connect();
+const PORT = process.env.PORT || 9000;
 
-    app.listen(PORT, () => {
-      console.log(
-        `Server running on port ${PORT}`
-      );
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-startServer();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
